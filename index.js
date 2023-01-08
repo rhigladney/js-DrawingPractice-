510d476d3cbbc5840ce8e87b7a7ac9d52608a052
rhigladney <71747665+rhigladney@users.noreply.github.com>
2023-01-07 22:45:11 -0800

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+ellipse(212, 206, 283, 318);
+
+ellipse(212, 250, 100, 73);
+
+ellipse(150, 150, 30, 30);
+
+ellipse(150, 150, 30, 30);
